082228ac3087cc5d66a627fdc7a4fce9b23364c5
Aleksey K <alexkoshernosiegov@gmail.com>
2022-03-30 23:02:05 +0300

    Added the simplest DB controller

---
+++ core/db/controller/pg_controller.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+import sqlalchemy as sa
+
+import core.context
+
+
+class PgController:
+    def __init__(self, context: core.context.Context):
+        self.__context = context
+        self.engine: Optional[sa.engine.Engine] = None
+
+    def __enter__(self):
+        self.engine = sa.create_engine(
+            self.__context.config.db.db_connection_string,
+            echo=False,
+        )
+        yield self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.engine is not None:
+            self.engine.dispose()
+            self.engine = None
