36e363a230bc93f4bf3701ab495802cbafa7c402
Aleksey K <alexkoshernosiegov@gmail.com>
2022-04-04 11:20:24 +0300

    [Docker] Initial creation of the python container

---
+++ assets/docker/set_locale.sh
@@ -0,0 +1,6 @@
+# Configuring locales
+# https://github.com/oerdnj/deb.sury.org/issues/56#issuecomment-93694233
+apt-get install -y locales
+locale-gen en_US.UTF-8
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
---
+++ assets/docker/set_tz.sh
@@ -0,0 +1,4 @@
+# Configuring TZ
+echo 'tzdata tzdata/Areas select Etc' | debconf-set-selections; \
+    echo 'tzdata tzdata/Zones/Etc select UTC' | debconf-set-selections; \
+    apt-get install -qqy --no-install-recommends  tzdata
