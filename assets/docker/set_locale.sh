eb41e6fe85dd35783bdf5d6c73bd24f3015c93d6
Aleksey K <alexkoshernosiegov@gmail.com>
2022-04-04 23:03:54 +0300

    [Docker] Base-line creation of the python container

--- assets/docker/set_locale.sh
+++ assets/docker/set_locale.sh
@@ -1,6 +1,6 @@
 # Configuring locales
 # https://github.com/oerdnj/deb.sury.org/issues/56#issuecomment-93694233
-apt-get install -y locales
+apt-get install -y locales locales-all
 locale-gen en_US.UTF-8
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
